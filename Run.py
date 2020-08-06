a580c616b71b9a9bcc45336b26ca91f8c0ac379e
Jeremy Tao <492350627@qq.com>
2020-08-06 15:46:11 +0800

    .

--- Run.py
+++ Run.py
@@ -2,6 +2,7 @@
 #-*-coding:utf-8 -*-
 import sqlite3
 import time
+# import random
 # import logging
 import GetData
 
@@ -9,8 +10,8 @@ class Run():
     def __init__(self,):
         #日志
         #logging.basicConfig(filename='logger.log', level=logging.INFO)
-        db = sqlite3.connect('./data.db')
-        self.cursor = db.cursor()
+        self.db = sqlite3.connect('./data.db')
+        self.cursor = self.db.cursor()
     def run(self,):
         time_set = time.localtime(time.time()) 
         cur_time = '{}-{}-{} {}:{}:{}'.format(
@@ -22,18 +23,24 @@ class Run():
             time_set[5]
             )
         while True:
-            time.sleep(2)
+            time.sleep(60)
             try:
-
-                humidity, temperature = GetData.get_wearther_data()
+                getdata = GetData.AccData()
+                humidity, temperature = getdata.get_wearther_data()
+                # humidity, temperature = self.test()
                 self.write_data(cur_time,humidity,temperature)
 
             except Exception as f:
                 print(f)
+        self.close()
 
     def write_data(self, time, humidity, temperature):
-        self.cursor.execute('insert into DATA values({},{},{});'.format(time, humidity, temperature))
-        self.close()
+        sql_ = 'insert into user values("{}","{}","{}")'.format(time, humidity, temperature)
+        print(sql_)
+        self.cursor.execute(sql_)
+        self.db.commit()
+    # def test(self,):
+    #     return int(random.random()*100), int(random.random()*100)
     # def get_oneday_data(param='today'):
     #     #获取今天的温度
     #     if param=='today':
